19c13b2d81a7728849424d48ff226c31b387e42e
duongnq2798 <duongnq27.98@gmail.com>
2021-02-04 21:45:59 +0700

    fix styling

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import {
   View,
   Text,
   ScrollView,
-  StyleSheet,
   StatusBar,
   TextInput,
   TouchableOpacity,
   Keyboard,
 } from 'react-native';
 import {data} from './data';
+import styles from './styles';
 
 const App = () => {
   const [toDoList, setToDoList] = useState(data);
@@ -19,7 +19,6 @@ const App = () => {
     let mapped = toDoList.map((task) => {
       return task.id == id ? {...task, complete: !task.complete} : {...task};
     });
-    console.log('delete');
     setToDoList(mapped);
   };
 
@@ -37,9 +36,7 @@ const App = () => {
   return (
     <View>
       <StatusBar backgroundColor="blue" />
-      <Text style={{fontSize: 20, marginVertical: 20, textAlign: 'center'}}>
-        ToDo List
-      </Text>
+      <Text style={styles.titleApp}>ToDo List</Text>
       <TextInput
         style={styles.addTask}
         placeholder="Add Task..."
@@ -77,34 +74,4 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 20,
-  },
-  addTask: {
-    borderRadius: 5,
-    borderColor: 'blue',
-    borderWidth: 2,
-    marginHorizontal: 10,
-    marginVertical: 10,
-  },
-  submit: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-    paddingVertical: 15,
-    marginHorizontal: 20,
-    marginVertical: 10,
-  },
-  titleTodoItem: {
-    fontSize: 18,
-  },
-  complete: {
-    width: '100%',
-    height: 5,
-    backgroundColor: 'red',
-    position: 'absolute',
-    top: 10,
-  },
-});
 export default App;
---
+++ styles.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  titleApp: {fontSize: 20, marginVertical: 20, textAlign: 'center'},
+  addTask: {
+    borderRadius: 5,
+    borderColor: 'blue',
+    borderWidth: 2,
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  submit: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    paddingVertical: 15,
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  titleTodoItem: {
+    fontSize: 18,
+  },
+  complete: {
+    width: '100%',
+    height: 5,
+    backgroundColor: 'red',
+    position: 'absolute',
+    top: 10,
+  },
+});
+
+export default styles;
